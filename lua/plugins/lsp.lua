4fb4ccd1a8fb94286f3f9ed0940415543acc320f
limjihoon <lonelynight1026@gmail.com>
2026-01-10 23:32:38 +0900

    refactor: restructure LSP configuration
    - Major refactoring of lua/plugins/lsp.lua
    - Remove nvim-treesitter.lua (functionality integrated elsewhere)
    - Reorganize LSP server configurations
    - Update lazy-lock.json dependencies
    - Improve LSP setup structure

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,89 +1,98 @@
--- LSP 설정 및 관련 플러그인
 return {
-  -- LSP 설정 (Neovim 0.11+ 내장 vim.lsp.config 사용)
-  {
-    'hrsh7th/nvim-cmp', -- 자동완성을 위한 메인 플러그인
-    event = { 'BufReadPre', 'BufNewFile', 'InsertEnter' },
-    dependencies = {
-      -- 자동완성 소스
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-
-      -- 스니펫 엔진 (nvim-cmp 필수)
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
+    {
+        "b0o/SchemaStore.nvim",
+        lazy = true,
+        version = false,
     },
-    config = function()
-      -- 공통 LSP 설정 및 키바인딩 로드
-      require('lsp')
-    end,
-  },
-
-  -- 스니펫 엔진
-  {
-    'L3MON4D3/LuaSnip',
-    version = 'v2.*',
-    build = 'make install_jsregexp',
-  },
-
-  -- 코드 포맷팅 플러그인
-  {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>cf',
-        function()
-          require('conform').format({ async = true, lsp_fallback = true })
+    {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        build = ':TSUpdate',
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                highlist = {
+                    enable = true,
+                },
+                indent = { enable = true },
+                autotage = { enable = true }, 
+                ensure_installed = {
+                    "bash",
+                    "html",
+                    "helm",
+                    "dockerfile",
+                    "java",
+                    "javascript",
+                    "json5",
+                    "lua",
+                    "markdown",
+                    "markdown_inline",
+                    "nix",
+                    "terraform", 
+                    "hcl",
+                    "python",
+                    "query",
+                    "regex",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "go", 
+                    "gomod", 
+                    "gowork", 
+                    "gosum",
+                    "yaml",
+                },
+            })
+        end
+    },
+    {
+        -- TODO : 어떻게 하면 mason 이 아닌 nix 패키지를 확인하고
+        -- nix 에서 찾을 수 없다면 에러가 발생하게 하지 ? 
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                pylsp = {},
+                jdtls = {},
+                clangd = {},
+                copilot = { enabled = false },
+                -- k8s lsp
+                -- Refered to following sources
+                -- https://www.reddit.com/r/neovim/comments/ze9gbe/kubernetes_auto_completion_support_in_neovim/
+                -- https://www.youtube.com/watch?v=pKCzpfqBbYs
+                yamlls = {
+                    settings = {
+                        yaml = {
+                            schemas = {
+                                kubernetes = "*.yaml",
+                                ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+                                ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+                                ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                                ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                                ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+                                ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+                                ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                                ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+                                ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+                                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+                                ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+                            },
+                        }
+                    }
+                }
+            },
+        },
+    },
+    {
+        "zbirenbaum/copilot.lua",
+        opts = function()
+            LazyVim.cmp.actions.ai_accept = function()
+                if require("copilot.suggestion").is_visible() then
+                    LazyVim.create_undo()
+                    require("copilot.suggestion").accept()
+                    return true
+                end
+            end
         end,
-        mode = '',
-        desc = '[Code] Format',
-      },
     },
-    opts = {
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        go = { 'goimports', 'gofmt' },
-        java = { 'google-java-format' },
-        nix = { 'alejandra' },
-        yaml = { 'prettier' },
-        javascript = { 'prettier' },
-        typescript = { 'prettier' },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-    },
-  },
-
-  -- 린팅 플러그인
-  {
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local lint = require('lint')
-
-      -- 실제로 설치된 린터만 설정 (KISS 원칙)
-      lint.linters_by_ft = {
-        go = { 'golangcilint' },
-        javascript = { 'eslint' },
-        typescript = { 'eslint' },
-        yaml = { 'yamllint' }, -- language.nix에 설치됨
-      }
-
-      -- 저장 및 텍스트 변경 시 자동 린트
-      vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave' }, {
-        callback = function()
-          -- 린터가 설치되어 있을 때만 실행
-          local ok = pcall(require('lint').try_lint)
-          if not ok then
-            -- 에러 무시 (린터 미설치)
-          end
-        end,
-      })
-    end,
-  },
 }
