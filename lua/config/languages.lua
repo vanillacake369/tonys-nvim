6b1d29715eb2cb142a165adcc247f91b9a695d7e
limjihoon <lonelynight1026@gmail.com>
2026-01-12 17:36:09 +0900

    refactor(languages): restructure language configuration for better organization

--- lua/config/languages.lua
+++ lua/config/languages.lua
@@ -1,253 +1,260 @@
 local M = {}
 
 M.languages = {
-    python = {
-        lsp_server = "pylsp",
-        lsp_opts = {
-            cmd = { "pylsp" },
-        },
-        treesitter = {
-            "python",
-            "ninja",
-            "rst",
-        },
-        linters = { "ruff" },
-        formatters = { "ruff_format", "black" },
-    },
-    java = {
-        lsp_server = "jdtls",
-        lsp_opts = {
-            cmd = { "jdtls" },
-        },
-        treesitter = {
-            "java",
-        },
-        formatters = { "google-java-format" },
-    },
-    c_cpp = {
-        lsp_server = "clangd",
-        lsp_opts = {
-            cmd = { "clangd" },
-        },
-        treesitter = {
-            "c",
-            "cpp",
-        },
-    },
-    yaml = {
-        lsp_server = "yamlls",
-        lsp_opts = {
-            cmd = { "yaml-language-server", "--stdio" },
-            settings = {
-                yaml = {
-                    schemas = {
-                        kubernetes = "*.yaml",
-                        ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-                        ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-                        ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-                        ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-                        ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-                        ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-                        ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-                        ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-                        ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-                        ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-                        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-                        ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-                    },
-                },
-            },
-        },
-        treesitter = {
-            "yaml",
-        },
-        linters = { "yamllint" },
-        formatters = { "prettier", "yamlfmt" },
-    },
-    go = {
-        lsp_server = "gopls",
-        lsp_opts = {
-            cmd = { "gopls" },
-        },
-        treesitter = {
-            "go",
-            "gomod",
-            "gowork",
-            "gosum",
-        },
-        linters = { "golangcilint" },
-        formatters = { "goimports", "gofmt" },
-    },
-    javascript = {
-        lsp_server = "ts_ls",
-        lsp_opts = {
-            cmd = { "typescript-language-server", "--stdio" },
-        },
-        treesitter = {
-            "javascript",
-            "typescript",
-            "tsx",
-        },
-        linters = { "biomejs" },
-        formatters = { "biome" },
-    },
-    nix = {
-        lsp_server = "nixd",
-        lsp_opts = {
-            cmd = { "nixd" },
-        },
-        treesitter = {
-            "nix",
-        },
-        linters = { "statix", "deadnix" },
-        formatters = { "alejandra" },
-    },
-    lua = {
-        lsp_server = "lua_ls",
-        lsp_opts = {
-            cmd = { "lua-language-server" },
-        },
-        treesitter = {
-            "lua",
-        },
-        linters = { "selene" },
-        formatters = { "stylua" },
-    },
-    terraform = {
-        lsp_server = "terraformls",
-        lsp_opts = {
-            cmd = { "terraform-ls", "serve" },
-        },
-        treesitter = {
-            "terraform",
-            "hcl",
-        },
-        linters = { "tflint" },
-        formatters = { "terraform_fmt" },
-    },
-    bash = {
-        lsp_server = "bashls",
-        lsp_opts = {
-            cmd = { "bash-language-server", "start" },
-        },
-        treesitter = {
-            "bash",
-        },
-        linters = { "shellcheck" },
-        formatters = { "shfmt" },
-    },
-    html = {
-        lsp_server = "html",
-        lsp_opts = {
-            cmd = { "vscode-html-language-server", "--stdio" },
-        },
-        treesitter = {
-            "html",
-        },
-        formatters = { "prettier" },
-    },
-    css = {
-        lsp_server = "cssls",
-        lsp_opts = {
-            cmd = { "vscode-css-language-server", "--stdio" },
-        },
-        treesitter = {
-            "css",
-        },
-        formatters = { "prettier" },
-    },
-    json = {
-        lsp_server = "jsonls",
-        lsp_opts = {
-            cmd = { "vscode-json-language-server", "--stdio" },
-        },
-        treesitter = {
-            "json",
-            "json5",
-        },
-        formatters = { "prettier" },
-    },
-    docker = {
-        lsp_server = "docker_compose_language_service",
-        lsp_opts = {
-            cmd = { "docker-compose-langserver", "--stdio" },
-        },
-        treesitter = {
-            "dockerfile",
-        },
-        linters = { "hadolint" },
-    },
-    helm = {
-        treesitter = {
-            "helm",
-        },
-        formatters = { "prettier" },
-    },
-    markdown = {
-        treesitter = {
-            "markdown",
-            "markdown_inline",
-        },
-        formatters = { "prettier" },
-    },
-    other = {
-        treesitter = {
-            "query",
-            "regex",
-            "vim",
-        },
-    },
+	python = {
+		lsp_server = "pylsp",
+		lsp_opts = {
+			cmd = { "pylsp" },
+		},
+		treesitter = {
+			"python",
+			"ninja",
+			"rst",
+		},
+		linters = { "ruff" },
+		formatters = { "ruff_format", "black" },
+	},
+	java = {
+		lsp_server = "jdtls",
+		lsp_opts = {
+			cmd = { "jdtls" },
+		},
+		treesitter = {
+			"java",
+		},
+		formatters = { "google-java-format" },
+	},
+	c_cpp = {
+		lsp_server = "clangd",
+		lsp_opts = {
+			cmd = { "clangd" },
+		},
+		treesitter = {
+			"c",
+			"cpp",
+		},
+	},
+	yaml = {
+		lsp_server = "yamlls",
+		lsp_opts = {
+			cmd = { "yaml-language-server", "--stdio" },
+			settings = {
+				yaml = {
+					schemas = {
+						kubernetes = "*.yaml",
+						["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+						["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+						["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+						["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+						["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+						["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+						["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+						["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+						["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+						["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+						["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+						["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+					},
+				},
+			},
+		},
+		treesitter = {
+			"yaml",
+		},
+		linters = { "yamllint" },
+		formatters = { "prettier", "yamlfmt" },
+	},
+	go = {
+		lsp_server = "gopls",
+		lsp_opts = {
+			cmd = { "gopls" },
+		},
+		treesitter = {
+			"go",
+			"gomod",
+			"gowork",
+			"gosum",
+		},
+		linters = { "golangcilint" },
+		formatters = { "goimports", "gofmt" },
+	},
+	javascript = {
+		lsp_server = "ts_ls",
+		lsp_opts = {
+			cmd = { "typescript-language-server", "--stdio" },
+		},
+		treesitter = {
+			"javascript",
+			"typescript",
+			"tsx",
+		},
+		linters = { "biomejs" },
+		formatters = { "biome" },
+	},
+	nix = {
+		lsp_server = "nixd",
+		lsp_opts = {
+			cmd = { "nixd" },
+			settings = {
+				nixd = {
+					formatting = {
+						command = { "alejandra" },
+					},
+				},
+			},
+		},
+		treesitter = {
+			"nix",
+		},
+		linters = { "statix", "deadnix" },
+		formatters = { "alejandra" },
+	},
+	lua = {
+		lsp_server = "lua_ls",
+		lsp_opts = {
+			cmd = { "lua-language-server" },
+		},
+		treesitter = {
+			"lua",
+		},
+		linters = { "selene" },
+		formatters = { "stylua" },
+	},
+	terraform = {
+		lsp_server = "terraformls",
+		lsp_opts = {
+			cmd = { "terraform-ls", "serve" },
+		},
+		treesitter = {
+			"terraform",
+			"hcl",
+		},
+		linters = { "tflint" },
+		formatters = { "terraform_fmt" },
+	},
+	bash = {
+		lsp_server = "bashls",
+		lsp_opts = {
+			cmd = { "bash-language-server", "start" },
+		},
+		treesitter = {
+			"bash",
+		},
+		linters = { "shellcheck" },
+		formatters = { "shfmt" },
+	},
+	html = {
+		lsp_server = "html",
+		lsp_opts = {
+			cmd = { "vscode-html-language-server", "--stdio" },
+		},
+		treesitter = {
+			"html",
+		},
+		formatters = { "prettier" },
+	},
+	css = {
+		lsp_server = "cssls",
+		lsp_opts = {
+			cmd = { "vscode-css-language-server", "--stdio" },
+		},
+		treesitter = {
+			"css",
+		},
+		formatters = { "prettier" },
+	},
+	json = {
+		lsp_server = "jsonls",
+		lsp_opts = {
+			cmd = { "vscode-json-language-server", "--stdio" },
+		},
+		treesitter = {
+			"json",
+			"json5",
+		},
+		formatters = { "prettier" },
+	},
+	docker = {
+		lsp_server = "docker_compose_language_service",
+		lsp_opts = {
+			cmd = { "docker-compose-langserver", "--stdio" },
+		},
+		treesitter = {
+			"dockerfile",
+		},
+		linters = { "hadolint" },
+	},
+	helm = {
+		treesitter = {
+			"helm",
+		},
+		formatters = { "prettier" },
+	},
+	markdown = {
+		treesitter = {
+			"markdown",
+			"markdown_inline",
+		},
+		formatters = { "prettier" },
+	},
+	other = {
+		treesitter = {
+			"query",
+			"regex",
+			"vim",
+		},
+	},
 }
 
 --- 범용 수집 함수 (Helper)
 local function collect_config(key, is_list)
-    local result = {}
-    for lang_name, config in pairs(M.languages) do
-        local data = config[key]
-        if data then
-            if is_list then
-                -- data가 테이블인지 확인하여 LSP 에러 방지
-                if type(data) == "table" then
-                    for _, v in ipairs(data) do
-                        table.insert(result, v)
-                    end
-                end
-            else
-                -- filetype별 매핑
-                local fts = config.filetypes or { lang_name }
-                for _, ft in ipairs(fts) do
-                    result[ft] = data
-                end
-            end
-        end
-    end
-    return result
+	local result = {}
+	for lang_name, config in pairs(M.languages) do
+		local data = config[key]
+		if data then
+			if is_list then
+				-- data가 테이블인지 확인하여 LSP 에러 방지
+				if type(data) == "table" then
+					for _, v in ipairs(data) do
+						table.insert(result, v)
+					end
+				end
+			else
+				-- filetype별 매핑
+				local fts = config.filetypes or { lang_name }
+				for _, ft in ipairs(fts) do
+					result[ft] = data
+				end
+			end
+		end
+	end
+	return result
 end
 
 -- 1. LSP: { [filetype] = opts } 매핑 반환 (lsp_server가 있는 경우만)
 function M.collect_lsp_servers()
-    local servers = {}
-    for _, config in pairs(M.languages) do
-        if config.lsp_server then
-            servers[config.lsp_server] = config.lsp_opts or {}
-        end
-    end
-    return servers
+	local servers = {}
+	for _, config in pairs(M.languages) do
+		if config.lsp_server then
+			servers[config.lsp_server] = config.lsp_opts or {}
+		end
+	end
+	return servers
 end
 
 -- 2. Treesitter: 모든 파서 이름을 하나의 리스트로 반환
 function M.collect_treesitter_parsers()
-    return collect_config("treesitter", true)
+	return collect_config("treesitter", true)
 end
 
 -- 3. Linters: { [filetype] = { linter1, ... } } 매핑 반환
 function M.collect_linters()
-    return collect_config("linters", false)
+	return collect_config("linters", false)
 end
 
 -- 4. Formatters: { [filetype] = { fmt1, ... } } 매핑 반환
 function M.collect_formatters()
-    return collect_config("formatters", false)
+	return collect_config("formatters", false)
 end
 
 return M
