fe939b54c596e0b3caee316f5b41c2307c140bb7
limjihoon <lonelynight1026@gmail.com>
2026-01-12 13:10:29 +0900

    feat(lsp): enhance server configuration with extended setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -198,13 +198,50 @@ end
 
 return {
     {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+    },
+    {
         "b0o/SchemaStore.nvim",
         lazy = true,
         version = false,
     },
     {
         "mfussenegger/nvim-lint",
-        event = { "BufReadPost", "BufNewFile" }, -- 표준 이벤트 사용
+        event = { "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged" }, -- 표준 이벤트 사용
         config = function()
             local lint = require("lint")
 
@@ -263,14 +300,8 @@ return {
                 },
                 underline = true,
                 signs = true,
-                update_in_insert = false,
-            })
-
-            -- 자동 팝업 설정
-            vim.api.nvim_create_autocmd("CursorHold", {
-                callback = function()
-                    vim.diagnostic.open_float(nil, { focusable = false })
-                end,
+                update_in_insert = true,
+                severity_sort = true,
             })
 
             vim.api.nvim_create_autocmd('LspAttach', {
