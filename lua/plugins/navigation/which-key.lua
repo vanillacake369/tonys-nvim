1eb1a6933b02e37087539e0c3d10dc4492a233c8
limjihoon <lonelynight1026@gmail.com>
2026-06-01 12:51:47 +0900

    style: apply stylua to project-wide Lua files
    Mechanical reformat of files that drifted from .stylua.toml.
    All diffs are whitespace-only (verified with git diff -w showing
    0 semantic changes). Unblocks CI lua-quality check.

--- lua/plugins/navigation/which-key.lua
+++ lua/plugins/navigation/which-key.lua
@@ -1,36 +1,36 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = function()
-		return {
-			preset = "modern",
-			-- Hide builtin g-prefix and noisy operator keymaps (gU, gu, g~)
-			filter = function(mapping)
-				local dominated = { gU = true, gu = true, ["g~"] = true, gw = true }
-				return not dominated[mapping.lhs]
-			end,
-			plugins = {
-				presets = {
-					g = false,
-				},
-			},
-			win = {
-				border = "rounded",
-				title = true,
-				title_pos = "center",
-				padding = { 2, 2 },
-				height = { min = 4, max = 25 },
-			},
-			spec = require("config.keymaps").get_which_key_spec(),
-		}
-	end,
-	keys = function()
-		local keymaps = require("config.keymaps")
-		local keys = keymaps.get_keys("which_key")
-		return keys
-	end,
-	config = function(_, opts)
-		local wk = require("which-key")
-		wk.setup(opts)
-	end,
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = function()
+        return {
+            preset = "modern",
+            -- Hide builtin g-prefix and noisy operator keymaps (gU, gu, g~)
+            filter = function(mapping)
+                local dominated = { gU = true, gu = true, ["g~"] = true, gw = true }
+                return not dominated[mapping.lhs]
+            end,
+            plugins = {
+                presets = {
+                    g = false,
+                },
+            },
+            win = {
+                border = "rounded",
+                title = true,
+                title_pos = "center",
+                padding = { 2, 2 },
+                height = { min = 4, max = 25 },
+            },
+            spec = require("config.keymaps").get_which_key_spec(),
+        }
+    end,
+    keys = function()
+        local keymaps = require("config.keymaps")
+        local keys = keymaps.get_keys("which_key")
+        return keys
+    end,
+    config = function(_, opts)
+        local wk = require("which-key")
+        wk.setup(opts)
+    end,
 }
