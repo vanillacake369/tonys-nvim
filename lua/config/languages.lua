f4bb3275550f350f1be668f43c03565ca04b2b94
limjihoon <lonelynight1026@gmail.com>
2026-07-19 14:46:15 +0900

    feat(yaml): load schemas from SchemaStore
    Replace the hand-maintained YAML schema map with SchemaStore-provided schemas while disabling yamlls' built-in schemaStore integration to avoid duplicate configuration.
    
    Keep Kubernetes schema matching explicit for common repository layout directories such as k8s, kubernetes, and manifests.

--- lua/config/languages.lua
+++ lua/config/languages.lua
@@ -52,24 +52,38 @@ M.languages = {
         lsp_server = "yamlls",
         lsp_opts = {
             cmd = { "yaml-language-server", "--stdio" },
-            filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab" },
+
+            filetypes = {
+                "yaml",
+                "yaml.docker-compose",
+                "yaml.gitlab",
+            },
+
             settings = {
                 yaml = {
-                    schemas = {
-                        kubernetes = "*.yaml",
-                        ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-                        ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-                        ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-                        ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-                        ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-                        ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-                        ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-                        ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-                        ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-                        ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-                        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-                        ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+                    schemaStore = {
+                        enable = false,
+                        url = "",
                     },
+
+                    schemas = (function()
+                        local schemas = require("schemastore").yaml.schemas()
+
+                        schemas.kubernetes = {
+                            "k8s/**/*.yaml",
+                            "k8s/**/*.yml",
+                            "kubernetes/**/*.yaml",
+                            "kubernetes/**/*.yml",
+                            "manifests/**/*.yaml",
+                            "manifests/**/*.yml",
+                        }
+
+                        return schemas
+                    end)(),
+
+                    validate = true,
+                    completion = true,
+                    hover = true,
                 },
             },
         },
