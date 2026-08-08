27b15a67053a7d9d50907037f62d36ada2fac4e0
limjihoon <lonelynight1026@gmail.com>
2026-08-08 15:44:14 +0900

    refactor(align): consume keymaps through bind
    Switch the align plugin spec to the shared keymaps.bind adapter without changing its mapped commands.

--- lua/plugins/core/easy-align.lua
+++ lua/plugins/core/easy-align.lua
@@ -3,7 +3,7 @@ return {
         "junegunn/vim-easy-align",
         event = "VeryLazy",
         keys = function()
-            return require("config.keymaps").get_keys("align")
+            return require("config.keymaps").bind("align")
         end,
         config = function() end,
     },
