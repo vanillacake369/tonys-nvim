617cd9ff64cd60eba64ba828c959dfbdc2eb16b6
limjihoon <lonelynight1026@gmail.com>
2026-08-08 15:44:15 +0900

    refactor(search): consume keymaps through bind
    Switch the search-replace plugin spec to the shared keymaps.bind adapter without changing its mapped commands.

--- lua/plugins/core/search-replace.lua
+++ lua/plugins/core/search-replace.lua
@@ -2,7 +2,7 @@ return {
     "nvim-pack/nvim-spectre",
     dependencies = { "folke/trouble.nvim", "nvim-mini/mini.icons" },
     keys = function()
-        return require("config.keymaps").get_keys("search_replace")
+        return require("config.keymaps").bind("search_replace")
     end,
     opts = {},
 }
