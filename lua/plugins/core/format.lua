8bcac8d4ce4321fc5bdb0f63f531d7f7323af193
limjihoon <lonelynight1026@gmail.com>
2026-08-08 15:44:14 +0900

    refactor(format): consume keymaps through bind
    Route the filtered format key subset through keymaps.bind so lazy loading still tracks only the format mapping.

--- lua/plugins/core/format.lua
+++ lua/plugins/core/format.lua
@@ -4,9 +4,12 @@ return {
     cmd = "ConformInfo",
     keys = function()
         -- PERF: code group 중 format key 만 conform lazy trigger 로 사용.
-        return require("config.keymaps").get_keys("code", function(item)
-            return item[1] == "<leader>cf"
-        end)
+        return require("config.keymaps").bind({
+            group = "code",
+            filter = function(item)
+                return item[1] == "<leader>cf"
+            end,
+        })
     end,
     opts = function()
         local lang = require("config.languages")
